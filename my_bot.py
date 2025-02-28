ddaa8a118902f081159115f4af087118c0493157
nikolaitilgreen <nikolai.tilgreen@gmail.com>
2025-02-28 16:32:11 +0100

    poker cheat codes

--- my_bot.py
+++ my_bot.py
@@ -14,7 +14,7 @@ class Bot:
       return BOT_NAME
 
   def act(self, obs: Observation):
-    # Your code here
+    self.win_probability(obs)
     return 0;
 
   def fold_or_check():
@@ -26,10 +26,44 @@ class Bot:
   def raise_amount(amount):
     return amount;
 
-  def win_probability(obs: Observation):
+  def win_probability(self, obs):
     current_bot_hand = obs.get_board_hand_type()
 
     current_hand_score = 0;
 
     if current_bot_hand == HandType.HIGHCARD:
       current_hand_score + 1;
+    elif current_bot_hand == HandType.PAIR:
+      current_hand_score + 2;
+    elif current_hand_score == HandType.TWOPAIR:
+      current_hand_score + 4;
+    elif current_hand_score == HandType.THREEOFAKIND:
+      current_hand_score + 6;
+    elif current_hand_score == HandType.STRAIGHT:
+      current_hand_score + 8;
+    elif current_hand_score == HandType.FLUSH:
+      current_hand_score + 10;
+    elif current_hand_score == HandType.FULLHOUSE:
+      current_hand_score + 12;
+    elif current_hand_score == HandType.FOUROFAKIND:
+      current_hand_score + 14;
+    elif current_hand_score == HandType.STRAIGHTFLUSH:
+      current_hand_score + 20
+    
+    my_hand = obs.my_hand
+
+    card1Rank = my_hand[0][1];
+    card2Rank = my_hand[1][1];
+
+    if obs.current_round <= 2:
+      if card1Rank == card2Rank:
+        board_cards = obs.board_cards
+        suits = [card[1] for card in board_cards]
+
+        filtered_suits = [suit for suit in suits if suit == card1Rank]
+        if len(filtered_suits) >= 2:
+          current_hand_score + 10
+      
+    return current_hand_score;
+      
+
